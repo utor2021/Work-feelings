265ab70642588e30a218d57ada604f11d0070bc6
lohith <lohith05amal@gmail.com>
2021-09-22 21:48:03 -0400

    index js update

--- models/index.js
+++ models/index.js
@@ -4,6 +4,23 @@ const Department = require('./department');
 const Status = require('./status');
 
 
+User.belongsTo(Department,{
+  foreignKey: 'department_id'
+});
+
+Department.hasMany(User,{
+  foreignKey: 'user_id'
+}); 
+
+User.hasMany(Status,{
+  foreignKey: 'status_id'
+});
+
+Status.belongsTo(User,{
+  foreignKey: 'user_id'
+});
+
+
 module.exports = {
   User,Department,Status
 };
