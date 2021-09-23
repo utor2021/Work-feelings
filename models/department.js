370ca1b6a11f07420e9e8d586fd64bdf8302b440
lohith <lohith05amal@gmail.com>
2021-09-22 21:45:09 -0400

    department db updated

--- models/department.js
+++ models/department.js
@@ -2,5 +2,24 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 
+Department.init(
+  {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,  
+      allowNull: false
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'department'
+  }
+)
 
 module.exports = Department;
