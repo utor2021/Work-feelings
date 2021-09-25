6d3b1c4c1447c41fb7f6478199f5a67ecc99f27b
mmifs <85654339+mmifs@users.noreply.github.com>
2021-09-25 13:42:14 -0400

    added new query for counting emojis on dashboh

--- seeds/status-seeds.js
+++ seeds/status-seeds.js
@@ -19,6 +19,21 @@ const statusdata = [
     },
     {
         emoji: 'sad',
+        diary: 'not feeling so good',
+        user_id: 3
+    },
+    {
+        emoji: 'angry',
+        diary: 'im pissed',
+        user_id: 3
+    },
+    {
+        emoji: 'happy',
+        diary: 'im bless',
+        user_id: 3
+    },
+    {
+        emoji: 'sad',
         diary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         user_id: 4
     },
