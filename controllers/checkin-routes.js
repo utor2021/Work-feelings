c6278fc8af5612b1b1345ecac6329692708bc26d
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-27 20:48:37 -0400

    accepted changes

--- controllers/checkin-routes.js
+++ controllers/checkin-routes.js
@@ -47,15 +47,15 @@ router.get('/dashboard', (req, res) => {
                 ],
                 group: ['emoji']
             })
-            .then(dbEmojiData => {
-                const emojiData = dbEmojiData.map(emojiCount => emojiCount.get({ plain: true }));
-                console.log(emojiData);
-                res.render('dashboard', { status, emojiData, title: 'Dashboard', loggedIn: true });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
-            });
+                .then(dbEmojiData => {
+                    const emojiData = dbEmojiData.map(emojiCount => emojiCount.get({ plain: true }));
+                    console.log(emojiData);
+                    res.render('dashboard', { status, emojiData, title: 'Dashboard', loggedIn: true });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
         })
         .catch(err => {
             console.log(err);
